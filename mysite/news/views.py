cdf2daecd8bd2f21c7ab595002fbea21e6dfa65b
NorthernBlow <evolution.is.not.theory@gmail.com>
2022-07-08 20:50:44 +0300

    idontknowhowitworks but I WORK

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 # Create your views here.
 def index(request):
+    news = News.objects.all()
+    res = '<h1>Список новостей</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p></div>\n<hr>\n'
     print(request)
-    return HttpResponse('fall i will follow')
+    return HttpResponse(res)
 def test(request):
     return HttpResponse('fuck')
